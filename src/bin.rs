5ad742bce8de915fe3e86c60ff565239657b4ac8
sm1215 <sm1215@gmail.com>
2021-02-07 23:04:47 -0500

    refactor cli args

--- src/bin.rs
+++ src/bin.rs
@@ -1,13 +1,13 @@
 use structopt::StructOpt;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use chrono::prelude::*;
 mod lib;
 
 #[derive(StructOpt, Debug)]
 struct Cli {
-    #[structopt(default_value = "z:/World of Warcraft", parse(from_os_str))]
-    source: std::path::PathBuf,
+    #[structopt(parse(from_os_str))]
+    source: Option<PathBuf>,
     #[structopt(short, long)]
     classic: bool,
     #[structopt(short, long)]
@@ -32,39 +32,54 @@ impl Mode {
 }
 
 #[derive(Debug)]
-// TODO: move source, destination here
 struct Snapshot {
     when: i64,
+    source: PathBuf,
+    destination: PathBuf,
+    targets: Vec<String>,
 }
 
-/// Describes the moment the backup is made
+/// Details describing the backup
 impl Snapshot {
+    fn new() -> Self {
+        Self {
+            when: Self::when(),
+            source: PathBuf::from("Z:\\World of Warcraft"),
+            destination: PathBuf::from("interface_backups"),
+            targets: vec![
+                String::from("Cache"),
+                String::from("Interface"),
+                String::from("WTF"),
+            ],
+        }
+    }
     fn when() -> i64 {
         let now = Utc::now();
         now.timestamp_millis()
     }
-
 }
 
 /// usage: intbak -- <wow_source>
 pub fn main() {
     println!("interface backup running. . . ");
+    let snapshot = Snapshot::new();
     let args = Cli::from_args();
-    let mut source_base = args.source.clone();
+    let mut source_base = match args.source {
+        Some(source) => source,
+        _ => snapshot.source
+    };
+    
     source_base.push(Mode::mode_path());
 
-    let mut destination_base = args.source.clone();
-    destination_base.push("interface_backups");
-    destination_base.push(Mode::mode_path());
-
-    let snapshot = Snapshot::when();
-    destination_base.push(&snapshot.to_string());
+    let mut destination_base = source_base.clone();
+    destination_base.push(snapshot.destination);
+    destination_base.push(Mode::mode_path());   
+    destination_base.push(snapshot.when.to_string());
 
     // TODO: setup these as a default,
     // optionally accept args from cli
     let targets = vec!["Cache", "Interface", "WTF"];
 
-    // TODO: pass a Snapshot over after source and dest are moved
     lib::run_backup(source_base, destination_base, targets, args.debug);
     println!("complete.");
 }
